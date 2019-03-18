a67915ed073a3b83c40565cfe225a88560446cf0
Radin Williams <radin.williams@ocsbstudent.ca>
2019-03-18 09:24:11 -0400

    added numeric text fields

---
+++ NumericTextFieldsRadinW/main.lua
@@ -0,0 +1,16 @@
+ --Title: NumericTextFields
+-- Name: RadinW
+-- Course: ICS2O/3C
+-- This program...-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+--hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- sets the background colour
+display.setDefault("background", 0/255, 0/255, 102/255)
