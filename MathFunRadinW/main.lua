4067a2d0805046f2f9dce229e8a891f712ba4c37
Radin Williams <radin.williams@ocsbstudent.ca>
2019-03-21 09:37:04 -0400

    added math fun

--- MathFunRadinW/main.lua
+++ MathFunRadinW/main.lua
@@ -1,13 +1,8 @@
- --Title: NumericTextFields
+ --Title: MathFun
 -- Name: RadinW
 -- Course: ICS2O/3C
 -- This program...-----------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
-
--- Your code here
+-------------------------------
 
 --hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
@@ -18,26 +13,40 @@ display.setDefault("background", 0/255, 0/255, 102/255)
 --create local variables
 local questionObject
 local correctObject
+local incorrectObject
 local NumericField
 local randomNumber1
 local randomNumber2
+local randomNumber3
+local randomNumber4
 local userAnswer
 local correctAnswer
 
 local function AskQuestion()
 	-- generate 2 random numbers numbers between a max. and a min. number
-	randomNumber1 = math.random(0, 10)
-	randomNumber2 = math.random(0, 10)
-    
-    correctAnswer = randomNumber1 + randomNumber2
-
+	randomNumber1 = math.random(1, 12)
+	randomNumber2 = math.random(1, 12)
+    randomNumber3 = math.random(1, 12)
+    randomNumber4 = math.random(1, 12)
+    correctAnswer = randomNumber1 + randomNumber2 
+ 
     -- create question in text object
-    questionObject.text = randomNumber1 .. " + " .. randomNumber2
+    questionObject.text = randomNumber1 .. " + " .. randomNumber1
+    questionObject.text = randomNumber2 .. " * " .. randomNumber2
+    questionObject.text = randomNumber3 .. " / " .. randomNumber3
+    questionObject.text = randomNumber4 .. " - " .. randomNumber4
 end
 
-local function HideCorrect()
+  local function HideCorrect()
+
     correctObject.isVisible = false
     AskQuestion()
+end
+
+local function Hideincorrect()
+    incorrectObject.isVisible = false
+    AskQuestion()
+
 end
 
 local function NumericFieldListener(event)    
@@ -57,6 +66,8 @@ local function NumericFieldListener(event)
          if (userAnswer == correctAnswer)  then
          	correctObject.isVisible = true
          	timer.performWithDelay(2000, HideCorrect)
+        else
+
         end
     end
 end    
@@ -64,9 +75,12 @@ end
 -- display a question and sets the coulor
 questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50)
 questionObject:setTextColor(155/255, 42/255, 198/255)
+
 correctObject = display.newText( "Correct!", display.contentWidth/4, display.contentHeight/3, nil, 60)
-correctObject.isVisible = true
 correctObject.isVisible = false
+
+incorrectObject = display.newText("incorrect!", display.contentWidth/4, 250, nil,160)
+incorrectObject.isVisible = false
 
 -- create numeric field
 numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80 )
